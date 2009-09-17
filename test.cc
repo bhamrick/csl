6862a3ea412e3e600a559e50fc51e9d9ec4f4f78
Brian Hamrick <2010bhamrick@ltsp2.csl.tjhsst.edu>
2009-09-17 10:55:40 -0400

    Test program takes matrix from input file

--- test.cc
+++ test.cc
@@ -7,13 +7,17 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	matrix m(2,3), p, q;
-	m.get(0,0) = 8;
-	m.get(0,1) = 4;
-	m.get(0,2) = 8;
-	m.get(1,0) = 4;
-	m.get(1,1) = 8;
-	m.get(1,2) = 4;
+	int N, M;
+	FILE *fin = fopen("matrix.in","r");
+	fscanf(fin,"%d%d",&N,&M);
+	matrix m(N,M), p, q;
+	for(int i = 0; i<N; i++) {
+		for(int j = 0; j<M; j++) {
+			int a;
+			fscanf(fin,"%d",&a);
+			m.get(i,j) = a;
+		}
+	}
 	cout<<m<<endl;
 	smithNormalForm(m,p,q);
 	cout<<m<<endl;

c0e084dac501d49a66cba70f8e13279424061cfb
Brian Hamrick <brian.c.hamrick@gmail.com>
2009-09-22 15:14:03 -0400

    Removed time binary and changed test.cc

--- test.cc
+++ test.cc
@@ -12,18 +12,15 @@ int main(int argc, char** argv) {
 	FILE *fin = fopen("matrix.in","r");
 	fscanf(fin,"%d%d",&N,&M);
 	mpz_matrix m(N,M), p, q;
-	matrix m2(N,M), p2, q2;
 	for(int i = 0; i<N; i++) {
 		for(int j = 0; j<M; j++) {
 			mpz_inp_str(m.get(i,j),fin,10);
-			m2.get(i,j) = mpz_get_si(m.get(i,j));
 		}
 	}
 	m.write(NULL);
 	smithNormalForm(m,p,q);
-	smithNormalForm(m2,p2,q2);
 	for(int i = 0; i<m.rows() && i < m.cols(); i++) {
-		gmp_printf("%Zd\t%d\n",m.get(i,i),m2.get(i,i));
+		gmp_printf("%Zd\n",m.get(i,i));
 	}
 	return 0;
 }
